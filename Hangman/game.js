32053d119c10a5d3697ddf65b3f9c28821fe90c2
Hristo <hmaleev@gmail.com>
2013-05-31 00:12:33 +0300

    New Game - Hangman

---
+++ Hangman/game.js
@@ -0,0 +1,21 @@
+var hiddenWord = "javascript";
+var hiddenWordLength = hiddenWord.length;
+var guessedDiv = document.getElementById("Guess");
+var dashes = "";
+var newDashes = "";
+var index = 0;
+guessedDiv.innerHTML = "";
+
+	for (var i = 0; i < hiddenWordLength; i++) {
+		dashes += "-"
+	};
+
+	guessedDiv.innerHTML = dashes;
+	var guess = "a"
+	var character;
+
+	for (var i = 0; i < hiddenWordLength; i++) {
+
+	};
+
+console.log(dashes);
