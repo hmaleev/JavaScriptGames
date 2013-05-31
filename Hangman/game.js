80a11df353f6f058c7aeb8c108d6f2943ffcb1cc
Hristo Maleev <hmaleev@gmail.com>
2013-05-31 16:36:18 +0300

    Updated Hangman
    added a new function to set character at a given position

--- Hangman/game.js
+++ Hangman/game.js
@@ -1,21 +1,40 @@
-var hiddenWord = "javascript";
+var hiddenWord = document.getElementById("hiddenWord").innerHTML;
 var hiddenWordLength = hiddenWord.length;
 var guessedDiv = document.getElementById("Guess");
 var dashes = "";
 var newDashes = "";
-var index = 0;
+
+var guess = new Array();
+guess[0] = "s";
+guess[1] = "a";
+guess[2] = "j";
+guess[3] = "v";
+
+
 guessedDiv.innerHTML = "";
+runGame();
+
+function runGame() {
 
 	for (var i = 0; i < hiddenWordLength; i++) {
 		dashes += "-"
 	};
 
 	guessedDiv.innerHTML = dashes;
-	var guess = "a"
-	var character;
 
 	for (var i = 0; i < hiddenWordLength; i++) {
+		for (var j = 0; j < hiddenWordLength; j++) {
+
+			if (hiddenWord.charAt(i) === guess[j]) {
+				dashes = setCharAt(i, guess[j], dashes);
+			}
+		};
 
 	};
+	guessedDiv.innerHTML = dashes;
+}
+//console.log(dashes);
 
-console.log(dashes);
+function setCharAt(index, character, string) {
+	return string.substring(0, index) + character + string.substring(index + character.length);
+}
