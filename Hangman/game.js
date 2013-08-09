584ca3a8d722a5fc69443c75fca9ac9819677407
hmaleev <hmaleev@gmail.com>
2013-08-09 17:21:39 +0300

    Hangman update
    added jQuery

--- Hangman/game.js
+++ Hangman/game.js
@@ -1,12 +1,57 @@
+
+var hiddenWord = document.getElementById("hiddenWord").innerHTML;
+var hiddenWordLength = hiddenWord.length;
+var guessedWord = new Array();
+var chars = new Array();
+
+for (var i = 0; i < hiddenWordLength; i++) {
+		guessedWord[i]="-";
+		
+	};
+	getSelection();
+var letter = $("input").value;
+console.log(letter);
+
+
+function guess(guessedWord,hiddenWord,letter) {
+
+	for(var i=0;i<hiddenWord; i++) {
+		if(letter===hiddenWord[i]) {
+			guessedWord[i]=letter;
+		}
+	}
+	return guessedWord;
+}
+function getSelection() {
+ $("button").click(function () {
+	var text = $(this).text();
+	console.log(text);
+	 $("input").val(text);
+	});
+	
+}
+
+
+
+
+
+
+
+/*
 var hiddenWord = document.getElementById("hiddenWord").innerHTML;
 var hiddenWordLength = hiddenWord.length;
 var guessedDiv = document.getElementById("Guess");
 var dashes = "";
 var newDashes = "";
-
+var hiddenWordAsArray = new Array();
+hiddenWordAsArray = hiddenWord.split("");
 
 function setCharAt(index, character, string) {
-	return string.substring(0, index) + character + string.substring(index + character.length);
+
+	var chars = new Array();
+	chars = string.split("");
+	//return string.substring(0, index) + character + string.substring(index + character.length);
+	return chars
 }
 
 
@@ -23,28 +68,38 @@ var letter = getSelection();
 console.log(letter);
 var guess = letter;
 
+for (var i = 0; i < hiddenWordLength; i++) {
+		dashes += "-"
+		
+	};
+	
+var dash=new Array();
+	dash = dashes.split("");
 function runGame() {
 
-	for (var i = 0; i < hiddenWordLength; i++) {
-		dashes += "-"
-	};
-
-	guessedDiv.innerHTML = dashes;
-	dashes = checkLetter(dashes);
-	var dashes2 = checkLetter(dashes);
+	var dashes2 = checkLetter(dash);
+	if(dashes2 != hiddenWord){
+		letter = getSelection();
+		dashes2 = checkLetter(dash);
+		runGame();
+	}
 	guessedDiv.innerHTML = dashes2;
 }
+
 function checkLetter(str) {
-	for (var i = 0; i < hiddenWordLength; i++) {
+	//for (var i = 0; i < hiddenWordLength; i++) {
 		for (var j = 0; j < hiddenWordLength; j++) {
 
-			if (hiddenWord.charAt(i) === guess) {
-				str = setCharAt(i, guess, str);
+			if (hiddenWordAsArray[j] === guess) {
+				str[j]=hiddenWordAsArray[j];
 			} else {
 				continue;
 			}
 		};
-	};
+	//};
 	return str;
 }
 runGame();
+*/
+
+
